262dde45b1bbce7c2935c9b37545e1a631e87b9d
Reuben Sivan <rsivan@sivansoftware.com>
2020-06-22 12:09:12 -0700

    this code reproduces "ERROR in Cannot read property 'loadChildren' of undefined"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { View1Component } from './view1/view1.component';
+import { View2Component } from './view2/view2.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/view1', pathMatch: 'full' },
+  { path: '', children: [
+  { path: 'view1', component: View1Component, data: { permission: (r: any) => r.guide > 0 }},
+  { path: 'view2', component: View2Component },
+  ] },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { View1Component } from './view1/view1.component';
+import { View2Component } from './view2/view2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    View1Component,
+    View2Component
   ],
   imports: [
     BrowserModule,
